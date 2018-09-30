06423edc9bcee7e3deb3b2b0e40e250dc0dbabec
Brayan Garzon <begarzonf@unal.edu.co>
2018-09-29 22:00:39 -0500

    registro

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.css';
+import Login from './components/Login';
+import Registro from './components/Registro';
+import App from './components/App';
+import Error from './components/Error';
+import registerServiceWorker from './registerServiceWorker';
+import {BrowserRouter, Route, Switch} from 'react-router-dom'
+
+ReactDOM.render(
+    <BrowserRouter>
+    <div>
+        <Switch>
+            <Route  path='/login' component={Login} exact/>
+            <Route  path='/signup' component={Registro} exact/>
+            <Route  path='/' component={App} exact/>
+            <Route  component={Error}/>
+        </Switch>
+    </div>
+        
+    </BrowserRouter>
+    , document.getElementById('root'));
+registerServiceWorker();
